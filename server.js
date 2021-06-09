b3552ae094f6b74ad257b8a53db2dcf74fcbeff1
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-06-09 07:48:04 +0200

    Style Bootstrap and jquery framework

--- server.js
+++ server.js
@@ -3,17 +3,16 @@ const { success, error } = require('consola')
 
 require('dotenv').config({ path: 'config.env' })
 
-
-
 const app = exp()
-app.set('views engine', 'ejs')
+app.set('view engine', 'ejs')
+app.use(exp.static('public'))
 
 const PORT = 5000 || process.env.PORT
 
-// Get /
+// Get 
 app.get('/', require('./routes/index'))
 
 app.listen(PORT, (err) => {
-    if (err) throw err
+    if (err) error({ message: `error ${err}`, badge: true })
     success({ message: `Server run on port: ${PORT}`, badge: true })
 })
