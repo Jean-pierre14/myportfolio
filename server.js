bbbf2c42af08305b40cf586146b6706d64601ffe
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-06-15 08:50:58 +0200

    Design UI

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const app = exp()
 app.set('view engine', 'ejs')
 app.use(exp.static('public'))
 
-const PORT = 5000 || process.env.PORT
+const PORT = 3000 || process.env.PORT
 
 // Get 
-app.get('/', require('./routes/index'))
+app.use('/', require('./routes/index'))
 
 app.listen(PORT, (err) => {
     if (err) error({ message: `error ${err}`, badge: true })
