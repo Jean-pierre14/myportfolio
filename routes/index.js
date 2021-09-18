9aa1d2f86d34261104b396a87046366bd1321dd6
User <chiruzabisimwa@gmail.com>
2021-09-18 15:27:49 +0200

    Set up new UI

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const exp = require('express')
 
 const router = exp.Router()
+const title = 'Bisimwa'
 
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', { title })
 })
 router.get('/card', (req, res) => {
     res.render('card')
